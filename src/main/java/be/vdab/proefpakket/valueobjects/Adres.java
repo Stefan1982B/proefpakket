1233fa54090e1ba14c7f40a40da6ecd5da3f8567
Stefaan <Stefaan@LOK111-04>
2018-09-13 15:46:06 +0200

    laatste dag

---
+++ src/main/java/be/vdab/proefpakket/valueobjects/Adres.java
@@ -0,0 +1,34 @@
+package be.vdab.proefpakket.valueobjects;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import be.vdab.proefpakket.entities.Gemeente;
+
+@Embeddable
+public class Adres implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String straat;
+	private String huisNr;
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "gemeenteId")
+	private Gemeente gemeente;
+
+	public String getStraat() {
+		return straat;
+	}
+
+	public String getHuisNr() {
+		return huisNr;
+	}
+
+	public Gemeente getGemeente() {
+		return gemeente;
+	}
+
+}
